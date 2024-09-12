777dd234991bc476cf827b241b670e481a07c4ac
toby044 <tobiasnolesen@gmail.com>
2024-09-12 09:54:58 +0200

    intersection-stuffs

--- frontend/components/PersonCard.vue
+++ frontend/components/PersonCard.vue
@@ -1,5 +1,5 @@
 <template>
-	<figure v-if="isMounted" class="c-person-card">
+	<figure class="c-person-card" ref="card">
 		<img
 			:src="img"
 			alt="A person"
@@ -13,14 +13,36 @@ defineProps({
 	name: String,
 });
 
-const isMounted = ref(false);
+const card = ref(null);
+const observer = ref(null);
+const isVisible = ref(false);
+
+function callback(e) {
+	isVisible.value = e[0].isIntersecting;
+	if (e[0].isIntersecting) {
+		e[0].target.classList.add('c-person-card--visible');
+	} else {
+		e[0].target.classList.remove('c-person-card--visible');
+	}
+}
 
 onMounted(() => {
-	isMounted.value = true;
+	observer.value = new IntersectionObserver(callback, {
+		rootMargin: '-50px',
+	});
+	observer.value.observe(card.value);
+});
+onBeforeUnmount(() => {
+	observer.value.disconnect();
 });
 </script>
 <style lang="postcss">
 :where(.c-person-card) {
-	@apply rounded-8px overflow-hidden cursor-pointer;
+	@apply rounded-8px overflow-hidden cursor-pointer opacity-0 -translate-y-24;
+	transition: all 0.3s var(--easing-ease-out-back);
+}
+
+.c-person-card.c-person-card--visible {
+	@apply opacity-100 translate-y-0;
 }
 </style>
